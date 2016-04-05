5d125e54bc309dc2d7e4f080f215aea9ee67565a
oliynuk25 <oliynuk25@gmail.com>
2016-04-05 20:09:53 +0300

    + user rest service

--- Hotel-project/src/main/java/ua/com/naukma/hotel/dao/UserRepository.java
+++ Hotel-project/src/main/java/ua/com/naukma/hotel/dao/UserRepository.java
@@ -2,7 +2,16 @@ package ua.com.naukma.hotel.dao;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ua.com.naukma.hotel.domain.model.User;
 
+import java.util.Collection;
+
 public interface UserRepository extends JpaRepository<User,Integer> {
+
+    @Query("SELECT u FROM User u JOIN u.reservations res where res.room.status = 'occupied' ")
+    Collection<User> getResidents();
+
+    @Query("SELECT u FROM User u JOIN u.reservations res where res.status = 'terminated' and YEAR(res.checkOut) >= YEAR(CURDATE())-1 ")
+    Collection<User> getBookingHistory();
 }
