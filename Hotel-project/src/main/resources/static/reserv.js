61b9842765c4b9e345a60a03a5951f8d0607a3b0
Angrytree <steainer@gmail.com>
2016-04-05 22:25:09 +0300

    reserv style

--- Hotel-project/src/main/resources/static/reserv.js
+++ Hotel-project/src/main/resources/static/reserv.js
@@ -3,10 +3,18 @@
  */
 var application = angular.module('appp', ['ngResource']);
 application.controller("reservController",function($scope, $resource) {
-    var Reservs  = $resource('/api/reservation');
+    var Reservs  = $resource('/api/reservation',{},{update : {method:"PUT"}});
     var reservs = Reservs.query(function() {
         $scope.reservs=reservs;
+        console.log($scope.reservs);
     });
+
+    $scope.accept= function(reserv){
+        reserv.$update();
+    } ;
+    $scope.cancel= function(reserv){
+        reserv.$update();
+    } ;
 
 
 });
--- Hotel-project/src/main/resources/static/room.js
+++ Hotel-project/src/main/resources/static/room.js
@@ -3,23 +3,47 @@ var application = angular.module('app', ['ngResource']);
 
 application.controller("RoomController",function($scope, $resource) {
     var Room  = $resource('/api/room');
-    var Type = $resource('/api/room');
     var rooms = Room.query(function(){
         $scope.rooms =  rooms;
     });
-    var types = Type.query(function(){
-        $scope.types =  types;
-    });
-
    $scope.update= function(room){
       room.$save();
-    }
+    } ;
 
     $scope.add=function(){
         console.log($scope.roo);
         $scope.rooms.push($scope.roo);
         Room.save($scope.roo);
-    }
+    } ;
+
+    $scope.roomTypes={
+        "Одномісний стандарт":"SINGLE_STANDARD",
+        "Одномісний покращений":"SINGLE_MODERATE",
+        "Одномісний люкс":"SINGLE_DELUXE",
+        "Двомісний стандарт":"DOUBLE_STANDARD",
+        "Двомісний покращений":"DOUBLE_MODERATE",
+        "Двомісний люкс":"DOUBLE_DELUXE",
+        "Трьохмісний стандарт":"TRIPLE_STANDARD",
+        "Трьохмісний покращений":"TRIPLE_MODERATE",
+        "Трьохмісний люкс":"TRIPLE_DELUXE",
+        "Чотирьохмісний стандарт":"QUAD_STANDARD",
+        "Чотирьохмісний люкс":"QUAD_DELUXE"
+    };
+    $scope.roomNiceTypes=[
+        "Одномісний стандарт",
+        "Одномісний покращений",
+        "Одномісний люкс",
+        "Двомісний стандарт",
+        "Двомісний покращений",
+        "Двомісний люкс",
+        "Трьохмісний стандарт",
+        "Трьохмісний покращений",
+        "Трьохмісний люкс",
+        "Чотирьохмісний стандарт",
+        "Чотирьохмісний люкс"
+    ];
+
+
 
 
 
