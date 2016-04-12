a9f180a926a8582bdc36963143c98d7fdd750a2a
oliynuk25 <oliynuk25@gmail.com>
2016-04-12 17:22:18 +0300

    update room status

--- Hotel-project/src/main/java/ua/com/naukma/hotel/dao/RoomRepository.java
+++ Hotel-project/src/main/java/ua/com/naukma/hotel/dao/RoomRepository.java
@@ -2,6 +2,7 @@ package ua.com.naukma.hotel.dao;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import ua.com.naukma.hotel.domain.model.Room;
@@ -18,8 +19,9 @@ public interface RoomRepository extends JpaRepository<Room,Integer> {
               "or res.checkOut between :checkIn AND  :checkOut or ( res.checkIn < :checkIn and res.checkOut > :checkOut)  ) ")
      Collection<Room> getAvailableRooms(@Param("checkIn") Date checkIn,@Param("checkOut")Date checkOut);
 
-     @Query("UPDATE Room r SET r.status = :status where r.id = (SELECT res.room.id FROM Reservation res where res.id=:id)")
-     void updateByStatus(@Param("id") int reservationId,@Param("status") RoomStatus status);
+    @Modifying
+    @Query("UPDATE Room r SET r.status = :status WHERE r.id = :id")
+     void updateByRoomStatus(@Param("id")int id,@Param("status") RoomStatus status);
 
     @Query("Select r FROM Room r JOIN r.reservations res WHERE r.status = 'OCCUPIED'and res.status='PROCESSED' and  DATEDIFF(res.checkOut,CURDATE()) <=1 ")
     Collection<Room> getRoomsWhichReleasedSoon();
